97d2f9e4cbea755429bf0463f26914ba3158fcf1
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-01-09 17:02:40 +0100

    Add abstract getFrequentRenterPoints

--- src/main/java/movierental/Price.java
+++ src/main/java/movierental/Price.java
@@ -1,49 +1,70 @@
 package movierental;
 
-public abstract class Price {
+public abstract interface Price {
+  abstract int getPriceCode();
+
   abstract double getCharge(int daysRented);
 
-  abstract int getPriceCode();
+  abstract int getFrequentRenterPoints(int daysRented);
 }
 
-class ChildrenPrice extends Price {
+class ChildrenPrice implements Price {
   @Override
-  int getPriceCode() {
+  public int getPriceCode() {
     return Movie.CHILDRENS;
   }
 
   @Override
-  double getCharge(int daysRented) {
+  public double getCharge(int daysRented) {
     double result = 1.5;
     if (daysRented > 3)
       result += (daysRented - 3) * 1.5;
     return result;
   }
+
+  @Override
+  public int getFrequentRenterPoints(int daysRented) {
+    return 1;
+  }
 }
 
-class NewReleasePrice extends Price {
+class NewReleasePrice implements Price {
   @Override
-  int getPriceCode() {
+  public int getPriceCode() {
     return Movie.NEW_RELEASE;
   }
 
   @Override
-  double getCharge(int daysRented) {
+  public double getCharge(int daysRented) {
     return daysRented * 3;
+  }
+
+  @Override
+  public int getFrequentRenterPoints(int daysRented) {
+    if (daysRented > 1) {
+      return 2;
+    } else {
+      return 1;
+    }
   }
 }
 
-class RegularPrice extends Price {
+class RegularPrice implements Price {
   @Override
-  int getPriceCode() {
+  public int getPriceCode() {
     return Movie.REGULAR;
   }
 
   @Override
-  double getCharge(int daysRented) {
+  public double getCharge(int daysRented) {
     double result = 2;
     if (daysRented > 2)
       result += (daysRented - 2) * 1.5;
     return result;
+  }
+
+  @Override
+  public int getFrequentRenterPoints(int daysRented) {
+    return 1;
   }
 }
