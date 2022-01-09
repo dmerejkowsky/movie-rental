7d578786b21390580472d86a8ee2543475296e9f
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-01-09 16:48:08 +0100

    Introduce Price abstract class

--- src/main/java/movierental/Movie.java
+++ src/main/java/movierental/Movie.java
@@ -7,19 +7,36 @@ public class Movie {
   public static final int REGULAR = 0;
 
   private String _title;
-  private int _priceCode;
+  private Price _price;
 
   public Movie(String title, int priceCode) {
     _title = title;
-    _priceCode = priceCode;
+    setPriceCode(priceCode);
   }
 
   public int getPriceCode() {
-    return _priceCode;
+    return _price.getPriceCode();
   }
 
   public void setPriceCode(int arg) {
-    _priceCode = arg;
+    switch (arg) {
+      case Movie.REGULAR: {
+        _price = new RegularPrice();
+        break;
+      }
+      case Movie.CHILDRENS: {
+        _price = new ChildrenPrice();
+        break;
+      }
+      case Movie.NEW_RELEASE: {
+        _price = new NewReleasePrice();
+        break;
+      }
+      default: {
+        throw new IllegalArgumentException("Invalid price code");
+      }
+
+    }
   }
 
   public String getTitle() {
@@ -56,5 +73,5 @@ public class Movie {
       return 1;
     }
   }
-  
+
 }
---
+++ src/main/java/movierental/Price.java
@@ -0,0 +1,26 @@
+package movierental;
+
+public abstract class Price {
+  abstract int getPriceCode();
+}
+
+class ChildrenPrice extends Price {
+  @Override
+  int getPriceCode() {
+    return Movie.CHILDRENS;
+  }
+}
+
+class NewReleasePrice extends Price {
+  @Override
+  int getPriceCode() {
+    return Movie.NEW_RELEASE;
+  }
+}
+
+class RegularPrice extends Price {
+  @Override
+  int getPriceCode() {
+    return Movie.REGULAR;
+  }
+}
