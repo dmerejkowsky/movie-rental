f4afb9b112eafe8839b0f2d01139a314e6aa245f
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-01-09 16:14:15 +0100

    Move amountsForEach into Rental

--- src/main/java/movierental/Rental.java
+++ src/main/java/movierental/Rental.java
@@ -8,7 +8,7 @@ public class Rental {
   private Movie _movie;
   private int _daysRented;
 
-    
+
   public Rental(Movie movie, int daysRented) {
     _movie = movie;
     _daysRented = daysRented;
@@ -20,5 +20,27 @@ public class Rental {
 
   public Movie getMovie() {
     return _movie;
+  }
+
+  //determine amounts for each line
+  double amountsForEach() {
+    double result = 0;
+
+    switch (getMovie().getPriceCode()) {
+      case Movie.REGULAR:
+        result += 2;
+        if (getDaysRented() > 2)
+          result += (getDaysRented() - 2) * 1.5;
+        break;
+      case Movie.NEW_RELEASE:
+        result += getDaysRented() * 3;
+        break;
+      case Movie.CHILDRENS:
+        result += 1.5;
+        if (getDaysRented() > 3)
+          result += (getDaysRented() - 3) * 1.5;
+        break;
+    }
+    return result;
   }
 }
